9da829d27058f436b08f9a510ed4549660b1369e
Yoav Lavi <yoavlavi122@gmail.com>
2021-02-20 21:17:47 +0100

    Fix version

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn main() {
 
 fn crispr() -> Result<(), &'static str> {
     let matches = App::new("crispr")
-        .version("1.0")
+        .version("0.2.0")
         .author("Yoav Lavi <yoavlavi122@gmail.com>")
         .about("Scaffolds a project from a template")
         .arg(
